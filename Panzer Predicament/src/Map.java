b8e06e57cebcecd7f7187684c78f06c6f0573fbf
Asdqwe258 <31545502+Asdqwe258@users.noreply.github.com>
2018-05-10 11:54:53 -0400

    Add files via upload
    updated map to be more compatible with pregenerated maps

--- Panzer Predicament/src/Map.java
+++ Panzer Predicament/src/Map.java
@@ -1,27 +1,41 @@
 public class Map {
 	private static int[][][] MAP_DATA;
+	private static int[][] PLAYER_1_SPAWN;
+	private static int[][] PLAYER_2_SPAWN;
 
 	public Map(int x, int y) {
 		MAP_DATA = new int[x][y][2];
-		for(int k = 0; k < MAP_DATA.length; k++){
-			for(int i = 0; i < MAP_DATA[0].length; i++){
-				for(int z = 0; z < MAP_DATA[0][0].length; z++){
-					if(Math.random() < .5){
-						generateBarrier(k,i,z);
+	}
+
+	public static void generateBarrier(int x, int y, int z) {
+		MAP_DATA[x][y][z] = 1;
+	}
+
+	public int[][][] getMapData() {
+		return MAP_DATA;
+	}
+
+	public int getBarrier(int x, int y, int z) {
+		return MAP_DATA[x][y][z];
+	}
+
+	public static void generateRandomMap() {
+		for (int k = 0; k < MAP_DATA.length; k++) {
+			for (int i = 0; i < MAP_DATA[0].length; i++) {
+				for (int z = 0; z < MAP_DATA[0][0].length; z++) {
+					if (Math.random() < .5) {
+						generateBarrier(k, i, z);
 					}
 				}
 			}
 		}
 	}
-
-	private static void generateBarrier(int x, int y,int z){
-		MAP_DATA[x][y][z] = 1;
+	
+	public static boolean generateSpawn(int[][][] map){
+		return false;
 	}
 	
-	public int[][][] getMapData(){
-		return MAP_DATA;
-	}
-	public int getBarrier(int x, int y, int z){
-		return MAP_DATA[x][y][z];
-	}
+	
+	
+	
 }
