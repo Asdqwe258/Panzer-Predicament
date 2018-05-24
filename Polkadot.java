82ff7b5bb68b07320195a5038ad3b372490fa124
Asdqwe258 <31545502+Asdqwe258@users.noreply.github.com>
2018-05-24 10:11:15 -0400

    moved tank on top of the ball

--- Polkadot.java
+++ Polkadot.java
@@ -107,7 +107,7 @@
 				if(getDiameter()!=5 && getDiameter()!=0)
 				{
 				ImageIcon tank = new ImageIcon("tank" + angle + ".png");
-				myBuffer.drawImage(tank.getImage(), (int)getX()-72, (int)getY()-72, null);
+				myBuffer.drawImage(tank.getImage(), (int)getX()-159, (int)getY()-163, null);
             buffer = myBuffer;
 				}
             
