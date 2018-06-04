533453c7d57fee996149a440992a6fdca77785c0
1435947 <39095111+1435947@users.noreply.github.com>
2018-06-04 08:18:52 -0400

    Add files via upload

--- PrizePanel.java
+++ PrizePanel.java
@@ -6,15 +6,16 @@
    import java.lang.Object;
    public class PrizePanel extends JPanel
    {
-      private static final int FRAME = 400; //size of map
+      private static final int FRAME = 1000; //size of map
       private static final Color BACKGROUND = new Color(204, 204, 204);
       private BufferedImage myImage;
       private Graphics myBuffer;
       private Polkadot pd;
-      private Ball ball;
+       private Ball ball, ball2, ball3, ball4, ball5;
+
       private Timer w;
       public ImageIcon t; 
-
+      public int firec=1;
       int hits = 0;
    	//constructor   
       public PrizePanel()
@@ -23,17 +24,35 @@
          myBuffer = myImage.getGraphics();
          myBuffer.setColor(BACKGROUND);
          myBuffer.fillRect(0, 0, FRAME,FRAME);
-         int xPos = (int)(Math.random()*(FRAME-100) + 50);
-         int yPos = (int)(Math.random()*(FRAME-100)+ 50);
-         pd = new Polkadot();
+       
+       
    
-      	 ball = new Ball(pd.getX(), pd.getY(), 5, Color.BLACK, 1);
          addKeyListener(new Key());
          setFocusable(true);
       	
          w = new Timer(0, new Listener());
          w.start();
-
+         pd = new Polkadot();
+         ball = new Ball(100, 100, 0, Color.BLACK, 0);
+         ball.setdx(0);
+         ball.setdy(0);
+         ball2 = new Ball(100, 100, 0, Color.BLACK, 0);
+         ball2.setdx(0);
+         ball2.setdy(0);
+         ball3 = new Ball(100, 100, 0, Color.BLACK, 0);
+         ball3.setdx(0);
+         ball3.setdy(0);
+         ball4 = new Ball(100, 100, 0, Color.BLACK, 0);
+         ball4.setdx(0);
+         ball4.setdy(0);
+         ball5 = new Ball(100, 100, 0, Color.BLACK, 0);
+         ball5.setdx(0);
+         ball5.setdy(0);
+         	ball.setfirenum(1);
+         	ball2.setfirenum(1);
+         	ball3.setfirenum(1);
+         	ball4.setfirenum(1);
+         	ball5.setfirenum(1);
       
       }
       public void paintComponent(Graphics g)
@@ -47,9 +66,17 @@
          {
             myBuffer.setColor(BACKGROUND);
             myBuffer.fillRect(0, 0, FRAME, FRAME);
-                 
-
-            ball.move(FRAME, FRAME);
+                   
+             ball.move(FRAME, FRAME);
+            ball2.move(FRAME, FRAME);
+            ball3.move(FRAME, FRAME);
+            ball4.move(FRAME, FRAME);
+            ball5.move(FRAME, FRAME);
+            ball.draw(myBuffer, pd.getAngle());
+            ball2.draw(myBuffer, pd.getAngle());
+            ball3.draw(myBuffer, pd.getAngle());
+            ball4.draw(myBuffer, pd.getAngle());
+            ball5.draw(myBuffer, pd.getAngle());
             pd.draw(myBuffer, pd.getAngle());
             ball.draw(myBuffer, pd.getAngle());
             myBuffer.setColor(Color.BLACK);
@@ -66,7 +93,57 @@
          {
                      if(e.getKeyCode() == KeyEvent.VK_SPACE)
                {
-
+  if(ball.getfirenum()==1)
+               {        
+					   ball.setAngle(pd.getAngle());
+                  ball.setX(pd.getX());
+                  ball.setY(pd.getY());
+                  ball.setcount(0);
+                  ball.setDiameter(5);
+                  ball.setdy((Math.cos((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(-100)));
+						ball.setdx((Math.sin((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(-100)));
+                  ball.setfirenum(0);
+               }
+               else if(ball2.getfirenum()==1)
+               {   
+					   
+                  ball2.setX(pd.getX());
+                  ball2.setY(pd.getY());
+                  ball2.setcount(0);
+                  ball2.setDiameter(5);
+                  ball2.setdy(-2);
+                  ball2.setfirenum(0);	  
+               }
+               else if(ball3.getfirenum()==1)
+               {  
+                  ball3.setX(pd.getX());
+                  ball3.setY(pd.getY());
+                  ball3.setcount(0);
+                  ball3.setDiameter(5);
+                  ball3.setdy(-2);
+                  ball3.setfirenum(0); 
+               }
+                  
+               else if(ball4.getfirenum()==1)
+               {                   
+                  ball4.setX(pd.getX());
+                  ball4.setY(pd.getY());
+                  ball4.setcount(0);
+                  ball4.setDiameter(5);
+                  ball4.setdy(-2);
+                  ball4.setfirenum(0); 
+                  	 
+               }
+               else if(ball5.getfirenum()==1)
+               { 
+                  ball5.setX(pd.getX());
+                  ball5.setY(pd.getY());
+                  ball5.setcount(0);
+                  ball5.setDiameter(5);
+                  ball5.setdy(-2);
+                  ball5.setfirenum(0); 
+               }
+            
 								 
          		}
             if(e.getKeyCode() == KeyEvent.VK_UP && pd.getY() > pd.getRadius()){
