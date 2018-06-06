85da14a82997974ab28364b5c9204d6255eef0ff
1435947 <39095111+1435947@users.noreply.github.com>
2018-06-06 13:11:05 -0400

    hi

--- PrizePanel.java
+++ PrizePanel.java
@@ -100,8 +100,8 @@
                   ball.setY(pd.getY());
                   ball.setcount(0);
                   ball.setDiameter(5);
-                  ball.setdy((-10)*(Math.cos((pd.getAngle()-1)/64.0 * (2 * Math.PI))));
-						ball.setdx((10)*(Math.sin((pd.getAngle()-1)/64.0 * (2 * Math.PI))));
+                  ball.setdy((Math.cos((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(-100)));
+						ball.setdx((Math.sin((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(-100)));
                   ball.setfirenum(0);
                }
                else if(ball2.getfirenum()==1)
@@ -147,13 +147,13 @@
 								 
          		}
             if(e.getKeyCode() == KeyEvent.VK_UP && pd.getY() > pd.getRadius()){
-                pd.setX(pd.getX()+ Math.sin(Math.toRadians(pd.getAngle()-1)*5.625 )*(10));   // move horizontally
-         pd.setY(pd.getY()+ Math.cos(Math.toRadians(pd.getAngle()-1)*5.625 )*(-10)); //move vertically
+                pd.setX(pd.getX()+ Math.sin((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(-10));   // move horizontally
+         pd.setY(pd.getY()+ Math.cos((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(-10)); //move vertically
          }  
             if(e.getKeyCode() == KeyEvent.VK_DOWN && pd.getY() < FRAME - pd.getRadius()){
-                pd.setX(pd.getX()+ Math.sin(Math.toRadians(pd.getAngle()-1)*5.625 )*(-10)); // move horizontally
+                pd.setX(pd.getX()+ Math.sin((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(10)); // move horizontally
         
-         pd.setY(pd.getY()+ Math.cos(Math.toRadians(pd.getAngle()-1)*5.625 )*(10)); //move vertically
+         pd.setY(pd.getY()+ Math.cos((double)(pd.getAngle()-1)/64 * (-2 * Math.PI))*(10)); //move vertically
            }
             if(e.getKeyCode() == KeyEvent.VK_LEFT && pd.getX() > pd.getRadius())
                pd.setAngle( pd.getAngle() - 1); 
